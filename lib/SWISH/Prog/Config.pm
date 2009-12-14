955fca733850d782b6581fba83f7b99fd96602bb
karpet <karpet@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2009-12-14 04:04:44 +0000

    5.10 fix

--- lib/SWISH/Prog/Config.pm
+++ lib/SWISH/Prog/Config.pm
@@ -745,7 +745,9 @@ KEY: for my $k ( sort keys %$config ) {
         for my $v ( @{ $conf3{$k} } ) {
             my $val  = $XML->escape( to_utf8($v) );
             my $note = '';
-            if ( exists $unsupported{$key} ) {
+
+            # $key fails to register in exists() below under 5.10
+            if ( exists $unsupported{$k} ) {
                 $note = $disclaimer;
                 $note =~ s/CONFIG/$key/;
             }
